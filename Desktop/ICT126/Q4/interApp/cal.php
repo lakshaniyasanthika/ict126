59391b1a8a3ac9a7580ad9d64e47368449a5c267
lakshaniyasanthika <lakshani97yasanthika@gmail.com>
2020-09-28 15:23:56 +0530

    firt

---
+++ Desktop/ICT126/Q4/interApp/cal.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* 
+*/
+class ClassName 
+{
+	public $n1;
+	public $n2;
+	public $total;
+	function __construct($n1,$n2,$total)
+	{
+		$this->n1=$n1;
+		$this->n2=$n2;
+		$this->total=$n1+$n2;
+	}
+}
+
+
+?>
---
+++ Desktop/ICT126/Q4/interApp/validate.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+</head>
+<body>
+<form name="frm" o="valid()"   method="post"></form>
+
+<h1>Enteruser name </h1><br>
+<input type="text" name="ip">
+
+<h1>Enteruser password </h1><br>
+<input type="text" name="pw">
+
+
+<input type="submit" name="submit" value="submit">
+
+</body>
+</html>
